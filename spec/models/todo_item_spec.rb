efe75c3fff2faa2f282fdb7ac192a9bb9b531ecd
Gabriel <hubermann@gmail.com>
2024-11-28 00:50:28 +0000

    Add model tests for TodoList and TodoItem

---
+++ spec/models/todo_item_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe TodoItem, type: :model do
+  let(:todo_list) { TodoList.create(name: "Example List") }
+
+  it "is valid with valid attributes" do
+    todo_item = TodoItem.new(name: "Example task", done: false, todo_list: todo_list)
+    expect(todo_item).to be_valid
+  end
+
+  it "is invalid without a name" do
+    todo_item = TodoItem.new(name: nil, todo_list: todo_list)
+    expect(todo_item).not_to be_valid
+  end
+end
---
+++ spec/models/todo_list_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe TodoList, type: :model do
+  it "is valid with a valid name" do
+    todo_list = TodoList.new(name: "My Todo List")
+    expect(todo_list).to be_valid
+  end
+
+  it "is invalid without a name" do
+    todo_list = TodoList.new(name: nil)
+    expect(todo_list).not_to be_valid
+  end
+
+  it "destroys associated todo_items when deleted" do
+    todo_list = TodoList.create(name: "List with items")
+    todo_list.todo_items.create(name: "Task 1", done: false)
+    todo_list.todo_items.create(name: "Task 2", done: true)
+
+    expect { todo_list.destroy }.to change { TodoItem.count }.from(2).to(0)
+  end
+end
